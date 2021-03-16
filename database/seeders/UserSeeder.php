e1c52ceadd579a3cb909364809ba4d3d32cf04f0
ACER\Z1402 <octaviaalya01@gmail.com>
2021-03-16 16:38:40 +0700

    Praktikum 3 membuat seeder dan migration tabel

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'name'=>'admin_baru',
+            'email'=>'adminbaru@admin.com',
+            'password'=> bcrypt('password'),
+        ]);
+    }
+}
